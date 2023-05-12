f8eeff20d10c7a48585e0c10e4d01d9b8b4010aa
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2023-05-12 16:15:23 +0545

    done with the logging part

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,9 @@
 import sys
 import os
-from src.logger import logging
+from logger import logging
 
 
-sys.path.append(os.path.abspath('src'))
+
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
