6336205c11a4a5baa2863b2397e5400e1567b397
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2023-05-13 20:11:32 +0545

    exception in components

--- src/components/data_tranformation.py
+++ src/components/data_tranformation.py
@@ -6,16 +6,16 @@ from sklearn.preprocessing import OrdinalEncoder #ordinal encoding
 ##for pipelines 
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer #to combine 2 different pipelines 
-
-
 import sys, os
 from dataclasses import dataclass
-
 import numpy as np
 import pandas as pd
 
-from src.exception import CustomException
+
+from src.components.exception1 import CustomException
 from src.logger import logging
+
+from src.utils import save_object
 
 # data transformation config 
 @dataclass 
@@ -94,13 +94,51 @@ class DataTransformation:
 
             logging.info("Read train and test data completed")
             logging.info(f'Train dataframe head:\n{train_df.head().to_string()}')
-            logging.info(f'Test dataframe head:\n{train_df.head().to_string()}')
+            logging.info(f'Test dataframe head:\n{test_df.head().to_string()}')
             
-            logging.info("Read train and test data completed")
+            logging.info("Obtaining preprocessing object")
 
             preprocessing_obj = self.get_data_transformation_object()
 
 
-        
+            target_column_name = 'price'
+            drop_columns = [target_column_name,'id']
 
+            #dividing features into independent and dependent feature
+
+            input_feature_train_df = train_df.drop(columns=drop_columns, axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+
+            input_feature_test_df = test_df.drop(columns=drop_columns, axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+        
+            # applying the transformation 
+            input_feature_train_array = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_array = preprocessing_obj.transform(input_feature_test_df)
+
+
+            logging.info("Applying preprocessing object on training and testing datasets")
+
+
+
+            train_arr = np.c_[input_feature_train_array, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_array, np.array(target_feature_test_df)]
+
+            save_object(
+                file_path= self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessing_obj
+            )
+
+            logging.info("Preprocessor pickel is created and saved")
+
+            return(
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
+        except Exception as e:
+            logging.info("Exception occured in the initiate_datatransformation")
+            raise CustomException(e, sys)
 
--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,45 @@
 # here we will all the code or functionality which will be common to all the project
+import os
+import sys
+from src.logger import logging
+from src.components.exception1 import CustomException
+import pandas as pd
+import numpy as np
+import pickle
+
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_model(X_train,Y_train, X_test,Y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+
+            #Train model
+            model.fit(X_train,Y_train)
+
+            #Predict Testing data
+            Y_test_pred = model.predict(X_test)
+
+            #Get R2 scores for train and test data
+            test_model_score = r2_score(Y_test, Y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    
+    except Exception as e:
+        logging.info("Exception occured during model training")
+        raise CustomException(e,sys)
